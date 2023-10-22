635c898e60e967fcd353e16f062fc25eb3a3799a
zmy15 <zmy15146892365@163.com>
2023-10-22 23:08:57 +0800

    fix:bug

--- nonebot_plugin_guess_number/__init__.py
+++ nonebot_plugin_guess_number/__init__.py
@@ -1,9 +1,13 @@
+import asyncio
 import random
 from nonebot import on_command
 from nonebot.exception import FinishedException
 from nonebot.internal.params import ArgPlainText
 from nonebot.typing import T_State
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent, Message
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, Message
+from nonebot import require
+
+require("nonebot-plugin-follow-withdraw")
 
 guess = on_command("猜数字", aliases={'cai', '猜猜看', '猜', }, priority=99, block=True)
 
@@ -21,9 +25,7 @@ async def handle(bot: Bot, event: MessageEvent, state: T_State):
 
 
 @guess.got("user_input")
-async def got(bot: Bot, event: GroupMessageEvent, user_input: str = ArgPlainText('user_input'), state=T_State):
-    message_id = event.message_id
-    bot_messages = state['bot_messages'].append(message_id)
+async def got(bot: Bot, event: MessageEvent, user_input: str = ArgPlainText('user_input'), state=T_State):
     if user_input.isdigit() and 1 <= int(user_input) <= 100:
         guess_number = int(user_input)
     elif user_input in ["取消", "退出", "结束", "不玩了", "exit"]:
@@ -35,7 +37,7 @@ async def got(bot: Bot, event: GroupMessageEvent, user_input: str = ArgPlainText
     max_ban_time = random.randint(1, 5)
     if guess_number != answer:
         if state['times'] == 5:
-            await delete_messages(bot, bot_messages)
+            await delete_messages(bot, state['bot_messages'])
             await guess.send('你已经用尽了5次机会，游戏结束。答案是{}。'.format(answer))
             await bot.set_group_ban(group_id=event.group_id, user_id=user_id, duration=60 * max_ban_time)
             msg = "恭喜您获得" + format_minutes(max_ban_time) + "的禁言"
@@ -44,12 +46,18 @@ async def got(bot: Bot, event: GroupMessageEvent, user_input: str = ArgPlainText
             state['times'] = state['times'] + 1
     try:
         if guess_number == answer:
-            await delete_messages(bot, bot_messages)
+            await delete_messages(bot, state['bot_messages'])
             await guess.finish('恭喜你猜对了！答案就是{}。'.format(answer))
         elif guess_number < answer:
-            await guess.reject('猜小了，再试试大一点的数字。')
+            await guess.send('猜小了，再试试大一点的数字。')
+            message_id = event.message_id
+            state['bot_messages'].append(message_id)
+            await guess.reject(None)
         else:
-            await guess.reject('猜大了，再试试小一点的数字。')
+            await guess.send('猜大了，再试试小一点的数字。')
+            message_id = event.message_id
+            state['bot_messages'].append(message_id)
+            await guess.reject(None)
     except ValueError:
         await guess.reject('请输入一个有效的整数。')
 
@@ -71,10 +79,10 @@ def format_minutes(minutes):
 
 
 # 撤回消息的函数
-async def delete_messages(bot: Bot, messages: list):
-    for message in messages:
-        message_id = message['message_id']
+async def delete_messages(bot: Bot, message_ids: list):
+    for message_id in message_ids:
         try:
             await bot.delete_msg(message_id=message_id)
+            await asyncio.sleep(random.randint(1, 3))
         except Exception as e:
             pass
