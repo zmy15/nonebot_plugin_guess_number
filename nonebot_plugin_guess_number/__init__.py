3f87f3ed0d2114824bf024e6ee27d4704e9b4ec5
zmy15 <zmy15146892365@163.com>
2023-10-24 13:02:41 +0800

    添加游戏限时，修复bug

--- nonebot_plugin_guess_number/__init__.py
+++ nonebot_plugin_guess_number/__init__.py
@@ -1,32 +1,53 @@
 import asyncio
 import random
+from datetime import datetime, timedelta
 from nonebot import on_command
 from nonebot.exception import FinishedException
 from nonebot.internal.params import ArgPlainText
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, Message
 
-
-guess = on_command("猜数字", aliases={'cai', '猜猜看' }, priority=99, block=True)
+guess = on_command("猜数字", aliases={'cai', '猜猜看'}, priority=99, block=True)
+player_last_game_time = {}
 
 
 @guess.handle()
 async def handle(bot: Bot, event: MessageEvent, state: T_State):
-    global answer
-    try:
-        await guess.send(f"猜一个1到100的整数，你有5次机会,猜错了会被禁言哦.输入 退出 来退出游戏，但会被禁言", at_sender=True)
-        answer = random.randint(1, 100)
-    except FinishedException:
-        pass
-    state['times'] = 1
-    state['bot_messages'] = []
+    global answer, user_id
+    user_id = event.get_user_id()
+    now = datetime.now()
+    hour = now.hour
+    if 7 <= hour <= 22:
+        await guess.finish(f"现在不是游戏时间,请在22点至次日7点进行游戏", at_sender=True)
+    else:
+        if state['player'] == 1:
+            await guess.finish(f"有人正在进行游戏，你现在不能进行游戏", at_sender=True)
+        elif user_id in player_last_game_time and now - player_last_game_time[user_id] < timedelta(minutes=10):
+            remaining_time = (player_last_game_time[user_id] + timedelta(minutes=10)) - now
+            await guess.finish(f"游戏休息中，请在{remaining_time.total_seconds():.0f}秒后再进行游戏。",
+                               at_sender=True)
+        elif user_id in player_last_game_time and now - player_last_game_time[user_id] > timedelta(minutes=10):
+            del player_last_game_time[user_id]
+        else:
+            try:
+                await guess.send(
+                    f"猜一个1到100的整数，你有5次机会,猜错了会被禁言哦.输入 退出 来退出游戏，但会被禁言",
+                    at_sender=True)
+                answer = random.randint(1, 100)
+                state['player'] = 1
+            except FinishedException:
+                pass
+        state['times'] = 1
+        state['bot_messages'] = []
 
 
 @guess.got("user_input")
 async def got(bot: Bot, event: MessageEvent, user_input: str = ArgPlainText('user_input'), state=T_State):
-    global user_id, max_ban_time
+    global max_ban_time
     if user_input.isdigit() and 1 <= int(user_input) <= 100:
         guess_number = int(user_input)
+    elif user_input in ["猜数字", "cai", "猜猜看"]:
+        await guess.reject(f"你已经在游戏中，请勿重复进行游戏", at_sender=True)
     elif user_input in ["取消", "退出", "结束", "不玩了", "exit"]:
         try:
             await bot.set_group_ban(group_id=event.group_id, user_id=user_id, duration=60 * max_ban_time)
@@ -37,6 +58,8 @@ async def got(bot: Bot, event: MessageEvent, user_input: str = ArgPlainText('use
             msg = "禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
             await guess.send(Message(f'{msg}'), at_sender=True)
         await guess.send("游戏退出", at_sender=True)
+        state['player'] = 0
+        player_last_game_time[user_id] = datetime.now()
         await delete_messages(bot, state['bot_messages'])
     else:
         guess_number = None
@@ -54,14 +77,17 @@ async def got(bot: Bot, event: MessageEvent, user_input: str = ArgPlainText('use
                 print(e)
                 msg = "禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
                 await guess.send(Message(f'{msg}'), at_sender=True)
+            state['player'] = 0
+            player_last_game_time[user_id] = datetime.now()
             await delete_messages(bot, state['bot_messages'])
         else:
             state['times'] = state['times'] + 1
-    user_id = event.get_user_id()
     max_ban_time = random.randint(1, 5)
     try:
         if guess_number == answer:
             await guess.send('恭喜你猜对了！答案就是{}。'.format(answer), at_sender=True)
+            state['player'] = 0
+            player_last_game_time[user_id] = datetime.now()
             await delete_messages(bot, state['bot_messages'])
         elif guess_number < answer:
             await guess.send('猜小了，再试试大一点的数字。', at_sender=True)
